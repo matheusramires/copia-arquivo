f9d6351df29a48e4d1b559ca0a582f2b21ea8e49
matheusramires <80134011+matheusramires@users.noreply.github.com>
2021-03-10 23:39:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 dir_origem = 'C:/DiretorioOrigem' #alterar para o diretorio onde o esta
-dir_destino = 'F:\DiretorioDestino' #alterar para o diretorio que deseja enviar
+dir_destino = 'F:/DiretorioDestino' #alterar para o diretorio que deseja enviar
 
 shutil.move(dir_origem,dir_destino)
 
